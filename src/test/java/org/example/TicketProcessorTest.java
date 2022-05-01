34b9170de6b9c6776b9add1840c6269e6747caba
Jorge Stief <jorge.stief@gmail.com>
2022-05-01 20:52:21 -0300

    TDD: test for what fails and adjust folders

---
+++ src/test/java/org/example/TicketProcessorTest.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import lombok.val;
+import org.example.domain.entities.Ticket;
+import org.example.domain.entities.Invoice;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+class TicketProcessorTest {
+    private TicketProcessor processor;
+
+    @BeforeEach
+    void setUp() {
+        processor = new TicketProcessor();
+    }
+
+    @Test
+    void fluxoNormal() {
+        val invoice = new Invoice("Jorge", LocalDateTime.now(), 100.0);
+
+        val values = new double[]{10.0, 60.0, 30.0};
+        val tickets = Arrays.stream(values).mapToObj(v -> new Boleto(v, LocalDateTime.now(), Double.toString(v))).collect(Collectors.toList());
+
+        String resultado = processor.avaliaPagamentos(invoice, tickets);
+
+        Assertions.assertEquals("PAGO", resultado);
+    }
+} 
